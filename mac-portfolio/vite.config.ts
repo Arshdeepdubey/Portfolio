485a5bf873e393fcf4b01ad1038a4dc3fae35551
Arshdeep Dubey <arshdeepdubey.ad@gmail.com>
2026-08-28 05:00:56 +0530

    fix: fix the page routing anf page path

--- mac-portfolio/vite.config.ts
+++ mac-portfolio/vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Use a relative path. This completely bypasses any case-sensitivity 
-  // issues with GitHub Pages repository names.
-  base: './', 
+  base: '/Portfolio/', 
 });
